6e7782ea50828228acb6100cd334fb9ee41da409
hiroki <hiroki12malu@gmail.com>
2022-10-12 00:18:06 +0900

    config修正

--- japaravel-front/nuxt.config.ts
+++ japaravel-front/nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  ssr :true,
+  app: {
+    head: {
+      title: "Japaravel",
+      meta: [
+        { charaset: "utf-8" },
+        { namae: "viewport", content: "width=device-width, user-scalable=yes,initial-scale=1.0"}
+      ],
+      link: [
+        { rel: "icon",type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "stylesheet",href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"}
+      ],
+    }
+  }
 })
