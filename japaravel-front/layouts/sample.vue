c48e9428d10dfa5365fa72cbd81480e5e27642d8
hiroki <hiroki12malu@gmail.com>
2022-10-12 00:17:53 +0900

    サンプルコードの追加

---
+++ japaravel-front/layouts/sample.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+      <p>サンプルレイアウト</p>
+      <slot />
+  </div>
+  </template>
---
+++ japaravel-front/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container-fluid">
+      <h1>Index</h1>
+      <NuxtLink to="/secondpage">to SecondPage</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "sample",
+});
+</script>
