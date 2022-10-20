55e1d99c9a33a5454894117b7d94cfd52119eb76
hiroki <hiroki12malu@gmail.com>
2022-10-20 22:50:35 +0900

    トップページメイン画像追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,43 @@
 <template>
-  <div class="container-fluid">
-    <h1>Index</h1>
-    <NuxtLink to="/secondpage">
-      to SecondPage
-    </NuxtLink>
+  <div>
+    <div class="">
+      <img
+        src="@/assets/images/main.jpeg"
+        :class="pcView ? 'pc-main': 'hideen'"
+        class="w-full rounded-lg object-cover lg:h-2/5"
+      >
+      {{ pcView }}
+    </div>
   </div>
 </template>
 
-<script setup>
-// eslint-disable-next-line no-undef
-definePageMeta({
-  layout: 'default',
-})
+<script>
+import { onMounted, ref } from 'vue'
+
+export default {
+  setup(){
+    const pcView = ref(true)
+    const windowWidth = ref(0)
+
+    const calculateWindowWidth = () => {
+      windowWidth.value = window.innerWidth
+      return pcView.value = windowWidth.value > 768
+    }
+
+    // resizeでウィンドウサイズ変更を検知
+    onMounted(() => {
+      window.addEventListener('resize', calculateWindowWidth)
+    })
+
+    return{
+      pcView,
+    }
+  }
+}
 </script>
+
+<style scoped>
+.pc-main {
+  height: 800px;
+}
+</style>
