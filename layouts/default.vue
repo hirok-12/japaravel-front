a2e8cd51dea3cee979df89a6a8108bd6b6fe0486
hiroki <hiroki12malu@gmail.com>
2022-10-24 23:59:39 +0900

    モーダル追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="w-11/12 mx-auto">
-    <Header />
+    <Modal
+      :title="タイトル"
+      :show="showModal"
+      @close="showModal = false"
+    >
+      ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+    </Modal>
+    <Header
+      :show="showModal"
+      @cutom="parentMethos"
+    />
     <slot />
     <Footer />
   </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const showModal = ref(false);
+
+    function parentMethos(e) {
+      showModal.value = e
+    }
+    return {
+      showModal,
+      parentMethos
+    };
+  },
+};
+</script> -->
