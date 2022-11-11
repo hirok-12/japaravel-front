14219dd960feb7882331605342522e68317465e6
hiroki <hiroki12malu@gmail.com>
2022-11-12 00:39:26 +0900

    ログインが必要なページではログインちゅうか事前チェックできるようにする

---
+++ middleware/auth.ts
@@ -0,0 +1,11 @@
+export default defineNuxtRouteMiddleware(async () => {
+  if (!process.server) {
+    const { checkAuthState, token } = useAuth()
+    const { infoToaste } = useNotify();
+    await checkAuthState()
+    if (!token.value) {
+      infoToaste('ログインが必要なページです')
+      return navigateTo('/login', { replace: true })
+    }
+  }
+})
