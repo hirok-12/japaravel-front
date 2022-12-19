2dce2d4fe4bbfc1042cfb87135f471008d1fdec0
hiroki <hiroki12malu@gmail.com>
2022-12-19 23:30:05 +0900

    トップページの改修

--- pages/index.vue
+++ pages/index.vue
@@ -2,121 +2,64 @@
   <div>
     <div class="">
       <img
-        src="@/assets/images/japan.jpeg"
+        src="@/assets/images/japaravel.png"
         :class="pcView ? 'pc-main' : ''"
         class="w-full rounded-lg object-cover lg:h-2/5"
       >
     </div>
     <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="text-center mb-20">
+      <div>
+        <div class="mt-3">
           <h1
-            class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4"
+            class="sm:text-3xl text-2xl font-medium title-font text-gray-900"
           >
-            Japaravelとは？🤔
+            探す🔍
           </h1>
-          <p
-            class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s"
-          >
-            Japarvelとは旅の思い出を記録するSNSです。47都道府県全ての思い出を残すとクリアになります。
-          </p>
-          <div class="flex mt-6 justify-center">
+          <div>
             <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
           </div>
         </div>
-        <div
-          class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6"
-        >
-          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-            <div
-              class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-10 h-10"
+      </div>
+      <div class="grid grid-cols-3 h-max gap-2">
+        <div class="mb-4">
+          <figure class="relative">
+            <a href="#">
+              <img
+                src="@/assets/images/japanese_food.jpeg"
+                class="rounded-lg h-32 md:h-80 lg:h-96"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-                />
-              </svg>
-            </div>
-            <div class="flex-grow">
-              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                STEP1
-              </h2>
-              <p class="leading-relaxed text-base">
-                自由気ままに日本全国を旅行しよう
-              </p>
-            </div>
-          </div>
-          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-            <div
-              class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-10 h-10"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
-                />
-              </svg>
-            </div>
-            <div class="flex-grow">
-              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                STEP2
-              </h2>
-              <p class="leading-relaxed text-base">
-                旅行先での素敵な思い出を記録しよう
-              </p>
-            </div>
-          </div>
-          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-            <div
-              class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-10 h-10"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
-                />
-              </svg>
-            </div>
-            <div class="flex-grow">
-              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                STEP3
-              </h2>
-              <p class="leading-relaxed text-base">
-                日本全国47都道府県全ての記録が完了すればクリア
-              </p>
-            </div>
-          </div>
+            </a>
+            <figcaption class="absolute bottom-6 px-2 text-lg text-white">
+              <p>食べる</p>
+            </figcaption>
+          </figure>
         </div>
-        <button
-          class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-        >
-          新規登録
-        </button>
+        <div class="mb-4">
+          <figure class="relative">
+            <a href="#">
+              <img
+                src="@/assets/images/culture.jpeg"
+                class="rounded-lg h-32 md:h-80 lg:h-96"
+              >
+            </a>
+            <figcaption class="absolute bottom-6 px-2 text-lg text-white">
+              <p>自然</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="mb-4">
+          <figure class="relative">
+            <a href="#">
+              <img
+                src="@/assets/images/nature.jpeg"
+                class="rounded-lg h-32 md:h-80 lg:h-96"
+              >
+            </a>
+            <figcaption class="absolute bottom-6 px-2 text-lg text-white">
+              <p>日本文化</p>
+            </figcaption>
+          </figure>
+        </div>
       </div>
     </section>
   </div>
