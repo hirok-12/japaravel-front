482f5556ac968f54f79e423430fe73a79bf0b1d5
hiroki <hiroki12malu@gmail.com>
2023-01-18 23:50:47 +0900

    削除機能の追加

---
+++ components/ConfirmModal.vue
@@ -0,0 +1,83 @@
+<template>
+  <div
+    id="overlay"
+    @click="clickEvent"
+  >
+    <div
+      id="content"
+      class="rounded-xl w-3/4 md:w-2/4"
+      @click="stopEvent"
+    >
+      <div class="text-center p-3 flex-auto justify-center leading-6">
+        <h2 class="text-xl py-4">
+          <slot />
+        </h2>
+      </div>
+      <div class="p-3 mt-2 text-center space-x-4 md:block">
+        <button
+          class="pryimary_btn"
+          @click="clickTrue"
+        >
+          はい
+        </button>
+        <button
+          class="secondary_btn"
+          @click="clickFalse"
+        >
+          いいえ
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  setup(props, context) {
+    const clickFalse = () => {
+      context.emit('from-child');
+    };
+
+    const clickTrue = () => {
+      context.emit('confrime-true');
+    };
+
+    const stopEvent = (event) => {
+      event.stopPropagation();
+    };
+    return {
+      stopEvent,
+      clickTrue,
+      clickFalse
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+#overlay{
+  /*要素を重ねた時の順番*/
+  z-index:10;
+
+  /*画面全体を覆う設定*/
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background-color:rgba(0,0,0,0.5);
+
+  /*画面の中央に要素を表示させる設定*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#content{
+  z-index:2;
+  padding: 1em;
+  background:#fff;
+}
+</style>
--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,5 +1,12 @@
 <template>
   <div class="mx-auto w-10/12">
+    <ConfirmModal
+      v-show="showConfirmModal"
+      @from-child="closeModal"
+      @confrime-true="deletePost(e)"
+    >
+      本当に削除しますか？
+    </ConfirmModal>
     <div>
       <div>
         <img
@@ -186,10 +193,34 @@
           </div>
         </NuxtLink>
       </div>
-      <TwitterShareButton
-        text="nuxtアプリ"
-        hash-tag="#nuxt"
-      />
+      <!-- 削除ボタン -->
+      <div class="my-6 mr-2">
+        <button
+          class="block border border-gray-300 p-3 rounded-md alert_btn"
+          to="/posts/4/edit"
+          @click="showDeleteModal()"
+        >
+          <div class="flex items-center space-x-4 justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 text-white"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+            <span
+              class="block w-max font-semibold tracking-wide text-white text-sm sm:text-base"
+            >削除</span>
+          </div>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -205,6 +236,7 @@ export default {
       google: null,
       marker: null,
       isMap: false,
+      showConfirmModal: false,
     };
   },
 
@@ -258,6 +290,15 @@ export default {
       .catch((e) => {
         console.error(e);
       });
+    },
+    showDeleteModal() {
+      this.showConfirmModal = true;
+    },
+    closeModal() {
+      this.showConfirmModal = false;
+    },
+    deletePost() {
+      // 削除処理
     },
   }
 };
